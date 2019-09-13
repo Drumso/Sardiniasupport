7c538ea44984cd8efbf9ae245c02b943c8e2a2a2
Fabio Colella <fcole90@gmail.com>
2019-09-13 18:48:54 +0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,20 +1,41 @@
 # A very simple Flask Hello World app for you to get started with...
+import os
+import logging
 
 from flask import Flask, render_template, request, json, send_from_directory
 import werkzeug
 import werkzeug.exceptions
 
-app = Flask(__name__)
+def create_app():
+    """Application factory function.
 
-# Intercepts generic server errors and logs them
-@app.errorhandler(werkzeug.exceptions.HTTPException)
-def handle_errors(e):
-#     logging.error(str(e))
-    return str(e), 500
+    This function generates the app instance of the backend and is useful to  assure that only one instance exists.
 
-@app.route('/')
-def default():
-    try:
-        return render_template('default.html')
-    except Exception as e:
-        return str(e)
+    It also retrieves settings from the configuration files to apply them to the application and contains the
+    functions related to the different API calls.
+
+    Returns
+    -------
+    app instance
+
+    """
+    
+    app = Flask(__name__)
+
+    # Intercepts generic server errors and logs them
+    @app.errorhandler(werkzeug.exceptions.HTTPException)
+    def handle_errors(e):
+    #     logging.error(str(e))
+        return str(e), 500
+
+    # Main page
+    @app.route('/')
+    def default():
+        try:
+            return render_template('default.html')
+        except Exception as e:
+            return str(e)
+   
+    # End of main function: 'create_app'
+    return app
+    
