ea1a52733ea98d881e664bb51761cc72f50c015d
Fabio Colella <fcole90@gmail.com>
2019-09-13 18:23:57 +0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,16 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, render_template, request, json, send_from_directory
+import werkzeug
+import werkzeug.exceptions
 
 app = Flask(__name__)
+
+# Intercepts generic server errors and logs them
+@app.errorhandler(werkzeug.exceptions.HTTPException)
+def handle_errors(e):
+    logging.error(str(e))
+    return str(e), 500
 
 @app.route('/')
 def hello_world():
