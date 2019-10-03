ea2a1c09dc23b78c9cf7c25ab8195581df172077
Fabio Colella <fcole90@gmail.com>
2019-10-04 00:00:33 +0300

    Initial view and template

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,13 @@
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
+
+from .models import Dish
+# ...
+def dish(request, dish_id):
+    try:
+        dish = Dish.objects.get(pk=dish_id)
+    except Dish.DoesNotExist:
+        raise Http404("Dish does not exist")
+    return render(request, 'recipes/dish.html', {'dish': dish})
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
